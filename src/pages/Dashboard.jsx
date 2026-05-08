e1f65f6e4d866645a7e465f8d7f0bcd582857d89
Phil Belinsky <philbelinsky@yahoo.com>
2026-05-08 10:22:28 -0400

    dashboard page UI completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,32 @@
+import {useEffect, useState} from "react";
+import {dummyAdminDashboardData, dummyEmployeeDashboardData} from "../assets/assets.jsx";
+import Loading from "../components/Loading.jsx";
+import EmployeeDashboard from "../components/EmployeeDashboard.jsx";
+import AdminDashboard from "../components/AdminDashboard.jsx";
 
 const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setData(dummyAdminDashboardData);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+    }, []);
+
+    if(loading) return (
+        <Loading/>
     )
+    if(!data) return (
+        <p className='text-center text-slate-500 py-12'>Failed to load dashboard</p>
+    )
+
+    if(data.role === "ADMIN") {
+        return <AdminDashboard data={data}/>
+    } else {
+        return <EmployeeDashboard data={data}/>
+    }
+
 }
 export default Dashboard
