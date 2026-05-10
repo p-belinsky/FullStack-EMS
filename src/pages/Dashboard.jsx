b100c08fda4a239cb00230d1f3d9ac3d1c6e0afb
Phil Belinsky <philbelinsky@yahoo.com>
2026-05-10 09:34:00 -0400

    attendance page UI completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setData(dummyAdminDashboardData);
+        setData(dummyEmployeeDashboardData);
         setTimeout(() => {
             setLoading(false);
         }, 1000);
